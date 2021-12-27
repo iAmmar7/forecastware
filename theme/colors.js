43a16f6c0bf72c03d518809372c1b8c1276e707d
Ammar Ansari <iammaransari@gmail.com>
2021-12-28 00:19:18 +0500

    light mode linear gradient colors updated

--- theme/colors.js
+++ theme/colors.js
@@ -1,14 +1,14 @@
 const colors = {
   primary: '#EC6E4C',
   accent: 'rgb(0, 122, 255)',
-  Clouds: ['#ffffff', '#81D4FA', '#29B6F6', '#039BE5'],
-  Haze: ['#ffffff', '#FFE082', '#FFC107', '#FF8F00'],
-  Clear: ['#ffffff', '#4FC3F7', '#03A9F4', '#039BE5'],
-  Thunderstorm: ['#ffffff', '#B0BEC5', '#90A4AE', '#546E7A'],
-  Drizzle: ['#ffffff', '#B2DFDB', '#4DB6AC', '#26A69A'],
-  Rain: ['#ffffff', '#90CAF9', '#42A5F5', '#2196F3', '#9E9E9E'],
-  Snow: ['#ffffff', '#C5CAE9', '#9FA8DA', '#8C9EFF'],
-  Default: ['#E0E0E0', '#9E9E9E', '#BDBDBD'],
+  Clouds: ['#ECEFF1', '#B0BEC5', '#90A4AE', '#78909C'], // Blue Grey
+  Haze: ['#FFFDE7', '#FFF59D', '#FFEE58', '#FBC02D'], // Yellow
+  Clear: ['#E3F2FD', '#90CAF9', '#42A5F5', '#2196F3'], // Blue
+  Thunderstorm: ['#D7CCC8', '#BCAAA4', '#A1887F', '#8D6E63'], // Brown
+  Drizzle: ['#B2DFDB', '#80CBC4', '#4DB6AC', '#26A69A'], // Teal
+  Rain: ['#FFCDD2', '#FFE0B2', '#FFF9C4', '#C8E6C9', '#B3E5FC', '#C5CAE9'], // Rainbow
+  Snow: ['#F0F4C3', '#E6EE9C', '#DCE775', '#D4E157'], // Lime
+  Default: ['#FAFAFA', '#E0E0E0', '#BDBDBD', '#9E9E9E'], // Grey
   linearText: 'rgb(28, 28, 30)',
   linearPlaceholder: 'rgba(0, 0, 0, 0.54)',
 };
