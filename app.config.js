403ff5bd2936a5a12053615cd4508013bd616235
Ammar Ansari <iammaransari@gmail.com>
2021-12-31 22:08:37 +0500

    sentry for production debugging initialized

--- app.config.js
+++ app.config.js
@@ -1,42 +1,33 @@
 import 'dotenv/config';
 
 export default {
-  // name: 'ForecastWare',
-  // version: '1.0.0',
-  // description: 'A weather forecast application',
-  // slug: 'forecastware',
-  // privacy: 'public',
-  // orientation: 'portrait',
-  // icon: './assets/appstore.png',
-  // githubUrl: 'https://github.com/iAmmar7/forecastware',
-  // userInterfaceStyle: 'automatic',
-  // splash: {
-  //   image: './assets/splash.png',
-  //   resizeMode: 'contain',
-  //   backgroundColor: '#ffffff',
-  // },
-  // updates: {
-  //   fallbackToCacheTimeout: 0,
-  // },
-  // assetBundlePatterns: ['assets/*'],
-  // ios: {
-  //   supportsTablet: true,
-  //   userInterfaceStyle: 'automatic',
-  //   bundleIdentifier: 'com.iammar7.forecastware',
-  //   buildNumber: '1.0.0',
-  // },
-  // android: {
-  //   adaptiveIcon: {
-  //     foregroundImage: './assets/playstore.png',
-  //     backgroundColor: '#ffffff',
-  //   },
-  //   userInterfaceStyle: 'automatic',
-  //   package: 'com.iammar7.forecastware',
-  //   versionCode: 1,
-  // },
-  // androidStatusBar: {
-  //   translucent: true,
-  // },
+  name: 'ForecastWare',
+  slug: 'forecastware',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: [],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: ['sentry-expo'],
   extra: {
     API_URL: process.env.API_URL,
     API_KEY: process.env.API_KEY,
